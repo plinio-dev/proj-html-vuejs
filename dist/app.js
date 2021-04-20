34e42c8795559e477ef5b72fd5adefe791229f73
Tommaso P <tommaso.plinio@gmail.com>
2021-04-21 00:23:02 +0200

    products visualizzati con vue js

--- dist/app.js
+++ dist/app.js
@@ -14,17 +14,34 @@ var app = new Vue({
     display: 'none',
     menuItems: ['Home', 'About US', 'Services', 'Shop', 'Our Team', 'Blog', 'Contact Us'],
     reviews: [{
-      id: '6-200x200_1',
+      img: '6-200x200_1',
       name: 'John Doe',
       text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi mollis eleifend ante ut sagittis. Sed sodales, urna et imperdiet placerat, ex nisi aliquam orci, ac varius odio libero et arcu.'
     }, {
-      id: '7-200x200_2',
+      img: '7-200x200_2',
       name: 'Pete Jones',
       text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi mollis eleifend ante ut sagittis. Sed sodales, urna et imperdiet placerat, ex nisi aliquam orci, ac varius odio libero et arcu.'
     }, {
-      id: '3-200x200_3',
+      img: '3-200x200_3',
       name: 'Mark Wilson',
       text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi mollis eleifend ante ut sagittis. Sed sodales, urna et imperdiet placerat, ex nisi aliquam orci, ac varius odio libero et arcu.'
+    }],
+    products: [{
+      img: 'img/brush_dark-400x400.png',
+      name: 'Brush',
+      prize: '$15.00'
+    }, {
+      img: 'img/scissors-400x400.png',
+      name: 'Scissors',
+      prize: '$85.00'
+    }, {
+      img: 'img/hot_oil_dark-400x400.png',
+      name: 'Hot Oil',
+      prize: '$15.00'
+    }, {
+      img: 'img/straight_razor_dark-400x400.png',
+      name: 'Straight Razor',
+      prize: '$30.00'
     }]
   },
   methods: {
