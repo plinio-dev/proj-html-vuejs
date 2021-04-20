1550549b0bdc63b4c0111a8ae066aa3da6091a36
Tommaso P <tommaso.plinio@gmail.com>
2021-04-20 23:56:45 +0200

    reviews visualizzate con vue js

--- src/app.js
+++ src/app.js
@@ -8,6 +8,23 @@ var app = new Vue ({
     display: 'none',
     menuItems: [
       'Home', 'About US','Services','Shop','Our Team','Blog','Contact Us',
+    ],
+    reviews: [
+      {
+        id: '6-200x200_1',
+        name: 'John Doe',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi mollis eleifend ante ut sagittis. Sed sodales, urna et imperdiet placerat, ex nisi aliquam orci, ac varius odio libero et arcu.',
+      },
+      {
+        id: '7-200x200_2',
+        name: 'Pete Jones',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi mollis eleifend ante ut sagittis. Sed sodales, urna et imperdiet placerat, ex nisi aliquam orci, ac varius odio libero et arcu.',
+      },
+      {
+        id: '3-200x200_3',
+        name: 'Mark Wilson',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi mollis eleifend ante ut sagittis. Sed sodales, urna et imperdiet placerat, ex nisi aliquam orci, ac varius odio libero et arcu.',
+      }
     ]
   },
 
